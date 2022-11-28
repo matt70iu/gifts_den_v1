2f6e3e547b68fde81a9095bc79ec0c2fe67a097c
matt70iu <mattadams60@gmail.com>
2022-11-28 15:38:11 +0000

    update env variables and secret key

--- gifts_den/settings.py
+++ gifts_den/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6pm)w(i66-zj9g7pwj_z1i6k2=1ls^t_6260p5grdz)o^k^e9r'
+SECRET_KEY = SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
