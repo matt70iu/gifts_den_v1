347637d8a72fe834e5fd334c37bf38f247e42a0f
matt70iu <mattadams60@gmail.com>
2022-12-13 04:08:12 +0000

    add checkout form data and tested payment intent in console and stripe

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -8,6 +9,24 @@ from products.models import Product
 from bag.contexts import bag_contents
 
 import stripe
+import json
+
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
@@ -18,8 +37,8 @@ def checkout(request):
         bag = request.session.get('bag', {})
 
         form_data = {
-            'first_name': request.POST['first_name'],
-            'last_name': request.POST['last_name'],
+            'name': request.POST['first_name'],
+            'name2': request.POST['last_name'],
             'email': request.POST['email'],
             'phone_number': request.POST['phone_number'],
             'country': request.POST['country'],
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -12,21 +12,23 @@ class StripeWH_Handler:
         Handle a generic/unknown/unexpected webhook event
         """
         return HttpResponse(
-            content=f'webhook received: {event["type"]}',
+            content=f'Unhandled webhook received: {event["type"]}',
             status=200)
 
     def handle_payment_intent_succeeded(self, event):
         """
-        Handle the payment intent.succeeded webhook from stripe.
+        Handle the payment_intent.succeeded webhook from Stripe
         """
+        intent = event.data.object
+        print(intent)
         return HttpResponse(
-            content=f'webhook received: {event["type"]}',
+            content=f'Webhook received: {event["type"]}',
             status=200)
 
     def handle_payment_intent_payment_failed(self, event):
         """
-        Handle the payment intent.payment_failed webhook from stripe.
+        Handle the payment_intent.payment_failed webhook from Stripe
         """
         return HttpResponse(
-            content=f'webhook received: {event["type"]}',
+            content=f'Webhook received: {event["type"]}',
             status=200)
