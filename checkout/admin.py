d01cb4f0044d111c7a8fbaac23ac1e64f2994e0e
matt70iu <mattadams60@gmail.com>
2022-12-13 20:53:54 +0000

    setup and test webhooks, fix form errors in views and correct js link in base.html

--- checkout/admin.py
+++ checkout/admin.py
@@ -11,13 +11,14 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
 
     readonly_fields = ('order_number', 'date',
-                       'delivery_cost', 'order_total', 'grand_total')
+                       'delivery_cost', 'order_total', 'grand_total', 'original_bag', 'stripe_pid',)
     fields = ('order_number', 'date', 'first_name', 'last_name', 'email', 'phone_number', 'country', 'postcode',
               'town_or_city', 'street_address1', 'street_address2', 'county', 'delivery_cost', 'order_total', 'grand_total',)
 
     list_display = ('order_number', 'date', 'first_name',
-                    'last_name', 'delivery_cost', 'order_total', 'grand_total',)
+                    'last_name', 'delivery_cost', 'order_total', 'grand_total', 'original_bag', 'stripe_pid',)
 
     ordering = ('-date',)
+
 
 admin.site.register(Order, OrderAdmin)
--- checkout/views.py
+++ checkout/views.py
@@ -37,8 +37,8 @@ def checkout(request):
         bag = request.session.get('bag', {})
 
         form_data = {
-            'name': request.POST['first_name'],
-            'name2': request.POST['last_name'],
+            'first_name': request.POST['first_name'],
+            'last_name': request.POST['last_name'],
             'email': request.POST['email'],
             'phone_number': request.POST['phone_number'],
             'country': request.POST['country'],
@@ -50,7 +50,11 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
@@ -83,6 +87,7 @@ def checkout(request):
         else:
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
+            print(order_form.errors)
     else:
         bag = request.session.get('bag', {})
         if not bag:
