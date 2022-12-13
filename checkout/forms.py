24f84452c68c34003d518061e583aca379d10af0
matt70iu <mattadams60@gmail.com>
2022-12-13 22:40:18 +0000

    add prfiles app and test template

--- checkout/forms.py
+++ checkout/forms.py
@@ -27,7 +27,6 @@ class OrderForm(forms.ModelForm):
             'street_address2': 'Street Address 2',
             'county': 'County',
         }
-        
 
         self.fields['first_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
