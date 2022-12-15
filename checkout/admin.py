15322051c0eaaa01ae71af454d1b2b52893df0f6
matt70iu <mattadams60@gmail.com>
2022-12-15 00:42:44 +0000

    add order history to profile and update checkout views to suit

--- checkout/admin.py
+++ checkout/admin.py
@@ -10,7 +10,7 @@ class OrderLineItemAdminInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
 
-    readonly_fields = ('order_number', 'date',
+    readonly_fields = ('order_number', 'user_profile', 'date',
                        'delivery_cost', 'order_total', 'grand_total', 'original_bag', 'stripe_pid',)
     fields = ('order_number', 'date', 'first_name', 'last_name', 'email', 'phone_number', 'country', 'postcode',
               'town_or_city', 'street_address1', 'street_address2', 'county', 'delivery_cost', 'order_total', 'grand_total',)
